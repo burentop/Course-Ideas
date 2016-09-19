66ea4a78042d951a89553397211349b3ec3b6920
Brent Perry <burentoperii@gmail.com>
2016-09-19 16:59:24 -0500

    Add voting buttons for ideas

--- src/main/java/com/teamtreehouse/courses/model/CourseIdea.java
+++ src/main/java/com/teamtreehouse/courses/model/CourseIdea.java
@@ -3,13 +3,17 @@ package com.teamtreehouse.courses.model;
 import com.github.slugify.Slugify;
 
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
 
 public class CourseIdea {
     private String slug;
     private String title;
     private String creator;
+    private Set<String> voters;
 
     public CourseIdea(String title, String creator) {
+        voters = new HashSet<>();
         this.title = title;
         this.creator = creator;
         try {
@@ -30,6 +34,14 @@ public class CourseIdea {
 
     public String getSlug() {
         return slug;
+    }
+
+    public boolean addVoter(String voterUserName) {
+        return voters.add(voterUserName);
+    }
+
+    public int getVoteCount() {
+        return voters.size();
     }
 
     @Override
